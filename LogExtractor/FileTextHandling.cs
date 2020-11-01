bb2152cb68875039111b43f340df6aa9ea32f754
Gadi Frenkel <GadiF@elmoil.local>
2020-11-01 17:51:16 +0200

    first commit

---
+++ LogExtractor/FileTextHandling.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LogExtractor
+{
+    class FileTextHandling
+    {
+        String myRexex = "TC[0-9]+ ";
+        //String myRexex = rexExpTextBox.Text;
+        string[] fileList = { @"C:\Users\gadif\Documents\SPIRA_PLUGIN\Latest_Logs\SQALAB1_Shell_TraceLog.txt" };
+        List<string> extractedTTxt = new List<string>();
+
+
+        public void matchRegExAfter(string RegX, String inputFile)
+        {
+            Regex _regex = new Regex(myRexex);
+
+            //foreach (string file in fileList)
+            //{
+                using (System.IO.StreamReader reader = new StreamReader(inputFile))
+                {
+                    //while ((line = reader.ReadLine()) != null)
+                    while (!reader.EndOfStream)
+                    {
+                        String currentLine = reader.ReadLine();
+                        // Try to match each line against the Regex.
+                        Match match = _regex.Match(currentLine);
+
+                        int idx = match.Index;
+
+                        if (match.Success && currentLine.Length > 0)
+                        {
+
+                            Console.WriteLine(currentLine.Substring(idx));
+                            extractedTTxt.Add(currentLine.Substring(idx));
+
+                        }
+                    }
+                }
+
+                //return "";
+            
+
+            //return null;
+        }
+
+        public void SaveToLog(string fileName)
+        {
+            using (System.IO.StreamWriter file =
+                //new System.IO.StreamWriter(@"\Users\gadif\Documents\SPIRA_PLUGIN\PlatinumDriveSmoke\SQALAB2\SQALAB2_Shell_TraceLog-EXTRACTED2", true))
+                new System.IO.StreamWriter(fileName, false))
+            {
+                foreach (string line in extractedTTxt)
+                {
+                    file.WriteLine(line);
+                }
+            }
+        }
+
+    }
+}
---
+++ LogExtractor/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogExtractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileTextHandling filehandling = new FileTextHandling();
+        string _sourceFileName;
+        public MainWindow()
+
+
+        {
+            InitializeComponent();
+              
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radioBtn_ExtractAfter(object sender, RoutedEventArgs e)
+        {
+            int t = 1;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string modifiedText;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                 _sourceFileName = openFileDialog.FileName;
+                modifiedText = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_Run_Click(object sender, RoutedEventArgs e)
+        {
+            string RegExTxt = this.rexExpTextBox.Text;
+
+            if (radiobutton_extract_before.IsChecked==true)
+            {
+
+            }
+            if (radiobutton_extract_after.IsChecked == true)
+            {
+                filehandling.matchRegExAfter(RegExTxt, _sourceFileName);
+            }
+            if (radiobutton_extract_this.IsChecked == true)
+            {
+
+            }
+            if (radiobutton_extract_remove_duplicates.IsChecked == true)
+            {
+
+            }
+
+            MessageBox.Show("Finished Extracting", "Log Extractor");
+        }
+
+        private void btn_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            string ExtractedAfter = _sourceFileName + "_Extracted_After.txt";
+            filehandling.SaveToLog(ExtractedAfter);
+            MessageBox.Show("Finished Saving Extracted File", "Log Extractor");
+
+        }
+    }
+
+        //private void btn_SaveAs_Click(object sender, RoutedEventArgs e)
+        //{
+        ////using (System.IO.StreamWriter file =
+        ////new System.IO.StreamWriter(@"\Users\gadif\Documents\SPIRA_PLUGIN\PlatinumDriveSmoke\SQALAB2\SQALAB2_Shell_TraceLog-EXTRACTED2", true))
+        ////{
+        ////    foreach (string line in extractedTTxt)
+        ////    {
+        ////        file.WriteLine(line);
+        ////    }
+        ////}
+        //Console.WriteLine("save as..");
+        //}
+    }
+
